f3213d01b1940b3fe820f0cc96002df332be743d
madisonlowe <mcourteney@live.com>
2022-08-20 09:18:13 +0100

    Updated data.js copy.

--- data.js
+++ data.js
@@ -29,7 +29,7 @@ RESTful API
 export const projects = [
   {
     title: "Seasonal",
-    desc: "A full-stack application written in Next.js, Node.js, Express and PostgreSQL, using both third-party and our own APIs. Designed and built by a small team of junior full-stack developers, we completed the MVP and one stretch goal within a month, before demoing the app to industry professionals. You can also see our presentation on YouTube.",
+    desc: "A full-stack application written in Next.js, Node.js, Express and PostgreSQL, using both third-party and our own APIs. Designed and built by a small agile team, we completed our MVP and first stretch goal within a month, and presented our product to other developers and industry professionals at a virtual showcase. A video of our presentation is linked in the GitHub repository.",
     github: "https://github.com/SchoolOfCode/final-project_front-end-12",
     tags: [
       "Team Project",
@@ -39,8 +39,14 @@ export const projects = [
     ],
   },
   {
+    title: "Couchbase Hackathon",
+    desc: "I attended a hackathon at the Couchbase Manchester office, where me and a small team created a full-stack application in a day, which could track moors fires (and in the future, other ecological disasters too). Our team won! It was great to get some experience using Couchbase and non-relational databases, which I had never used before this hackathon.",
+    github: "https://github.com/madisonlowe/couchbaseHackathon",
+    tags: ["Team Project", "Full-stack", "React", "Hackathon"],
+  },
+  {
     title: ".Class",
-    desc: "A full-stack application written in React, Node.js, Express and PostgreSQL. In our ninth week of learning how to code, me and three other teammates came together to build this MVP in under a week, before presenting our product to a panel of senior developers and industry professionals.",
+    desc: "A full-stack application written in React, Node.js, Express and PostgreSQL. In our ninth week of learning how to code, myself and three other teammates came together to build our MVP in under a week, before presenting our product to a panel of senior developers and industry professionals for questions and feedback.",
     github: "https://github.com/SchoolOfCode/w9_frontend-project-room-21-1",
     tags: [
       "Team Project",
@@ -50,32 +56,26 @@ export const projects = [
     ],
   },
   {
-    title: "Couchbase Hackathon",
-    desc: "I attended a hackathon at the Couchbase Manchester office, where me and a small team created a full-stack application in a day, to track moors fires and other ecological disasters. We won! It was great to get some practice in using Couchbase and non-relational databases, which I had never used before this hackathon.",
-    github: "https://github.com/madisonlowe/couchbaseHackathon",
-    tags: ["Team Project", "Full-stack", "React", "Hackathon"],
-  },
-  {
-    title: "Site 1.0",
-    desc: "The first version of my personal website, built in vanilla JavaScript.",
-    github: "https://github.com/madisonlowe/mySiteVanilla",
-    tags: ["Personal Project", "Front-end", "HTML / CSS / JavaScript"],
-  },
-  {
     title: "Site 2.0",
-    desc: "The second version of my personal website, rebuilt in React.",
+    desc: "The second version of my personal website, rebuilt in React. Rebuilding my website provided me with an opportunity to get some more experience working with React, which I didn't have a lot of experience with at the time. It was really fun to work with, though routing and deployment to GitHub provided their own challenges and drawbacks, which is how I ended up rebuilding the site again in Next.js (which is what you're reading now)!",
     github: "https://github.com/madisonlowe/mySiteReact",
     tags: ["Personal Project", "Front-end", "React / CSS"],
   },
   {
     title: "To Do List",
-    desc: "A to-do list app, built in React.",
+    desc: "A simple to-do list app, built in React. This project was the first thing I built in React, and it provided a great learning exercise to get to grips with using state and props.",
     github: "https://github.com/madisonlowe/toDoList",
     tags: ["Personal Project", "Front-end", "React"],
   },
   {
+    title: "Site 1.0",
+    desc: "The first version of my personal website, built in vanilla JavaScript. Primarily, this project provided an opportunity to practice working through UX/UI design cycles and get some experience with the fundamentals. The process of designing my original product - the website - according to a given brief is documented further on GitHub.",
+    github: "https://github.com/madisonlowe/mySiteVanilla",
+    tags: ["Personal Project", "Front-end", "HTML / CSS / JavaScript"],
+  },
+  {
     title: "Rock, Paper, Scissors!",
-    desc: "The first game I ever built in JavaScript! A small browser game with light and dark mode toggle.",
+    desc: "The first game I ever built in JavaScript! A small browser game with light and dark mode toggle. I built the first version of this game in my first week working with JavaScript, and continued to improve it and add to it as a way to practice HTML, CSS and JavaScript earlier in my learning journey.",
     github: "https://github.com/madisonlowe/RPS",
     tags: ["Personal Project", "Front-end", "HTML / CSS / JavaScript"],
   },
