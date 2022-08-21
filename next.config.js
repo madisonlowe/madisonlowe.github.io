52e9e2df3aa08881e3473f7c266651704fc381da
madisonlowe <mcourteney@live.com>
2022-08-21 13:02:22 +0100

    Added config back?'

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-/* next.config.js  */
-// module.exports = {
-//   i18n: {
-//     locales: ["en-gb"],
-//     defaultLocale: "en-gb",
-//   },
-// };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
+
+module.exports = nextConfig;
