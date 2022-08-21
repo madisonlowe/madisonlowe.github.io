eac6767fc44307d50dd69f559dad7fbb3fab071f
madisonlowe <mcourteney@live.com>
2022-08-21 12:14:24 +0100

    Removed i18n as build failing.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /* next.config.js  */
-module.exports = {
-  i18n: {
-    locales: ["en-gb"],
-    defaultLocale: "en-gb",
-  },
-};
+// module.exports = {
+//   i18n: {
+//     locales: ["en-gb"],
+//     defaultLocale: "en-gb",
+//   },
+// };
